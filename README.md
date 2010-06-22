7ba9bb1ef0b73feffed82de02706651d55a1821f
Dr Nic Williams <drnicwilliams@gmail.com>
2010-06-23 06:44:00 +1000

    WIP: Deploy command is now dynamic; though not working due to issue loading 'engineyard' gem + Escape class

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ To install via Git:
 
     mkdir -p ~/Library/Application\ Support/TextMate/Bundles
     cd ~/Library/Application\ Support/TextMate/Bundles
-    git clone git://github.com/engineyard/engine-yard.tmbundle.git "Engine Yard.tmbundle"
+    git clone git://github.com/engineyard/engineyard.tmbundle.git "Engine Yard.tmbundle"
     osascript -e 'tell app "TextMate" to reload bundles'
 
-Source can be viewed or forked via GitHub: [http://github.com/engineyard/engine-yard.tmbundle](http://github.com/engineyard/engine-yard.tmbundle)
+Source can be viewed or forked via GitHub: [http://github.com/engineyard/engineyard.tmbundle](http://github.com/engineyard/engine-yard.tmbundle)
---
+++ Support/bin/ey_command.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+# Render output header
+view = File.dirname(__FILE__) + "/../views/results_prefix.html"
+print File.read(view)
+$stdout.flush
+
+require "fileutils"
+begin
+  require "rubygems"
+  require "engineyard/cli"
+rescue LoadError => e
+  puts <<-HTML
+  </pre>
+  <div class="message">
+    <div class="error">Please install engineyard gem</div>
+    <pre>gem install engineyard</pre>
+  </div>
+  HTML
+  exit
+end
+
+unless ENV['TM_PROJECT_DIRECTORY'] && File.exist?(ENV['TM_PROJECT_DIRECTORY'])
+  puts <<-HTML
+  </pre>
+  <div class="message">
+    <div class="error">Run only from a project</div>
+    <p>The Engine Yard commands are for git-based, rack/rails projects</p>
+  </div>
+  HTML
+  exit
+end  
+
+ENV['THOR_SHELL'] = 'HTML'
+FileUtils.cd(ENV['TM_PROJECT_DIRECTORY'])
+
+begin
+  EY::CLI.start
+rescue EY::Error => e
+  EY.ui.print_exception(e)
+  exit(1)
+rescue Interrupt => e
+  puts
+  EY.ui.print_exception(e)
+  EY.ui.say("Quitting...")
+  exit(1)
+end
+
+# command = ARGV.shift
+# case command.to_sym
+# when :deploy
+# when :rebuild
+# when :logs
+# end
+# 
