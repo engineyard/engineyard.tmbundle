2c09706a10528d1d7dcde57b84182447133051da
Dr Nic Williams <drnicwilliams@gmail.com>
2010-06-23 13:35:33 +1000

    refactor ey_command bin into lib helper methods

--- Support/bin/ey_command.rb
+++ Support/bin/ey_command.rb
@@ -1,53 +1,9 @@
 #!/usr/bin/env ruby
 
-# Due to need for escape gem, which name clashes with Textmate Support libs,
-# Moving the Text
-tm_support = $:.select { |path| path =~ %r{SharedSupport/Support/lib} }.first
-$:.reject! { |path| path =~ %r{SharedSupport/Support/lib} }
-$: << tm_support if tm_support
+require "ey_command_helper"
+include EyCommandHelper
 
-# Render output header
-view = File.dirname(__FILE__) + "/../views/results_prefix.html"
-print File.read(view)
-$stdout.flush
-
-require "fileutils"
-begin
-  require "rubygems"
-  require "engineyard/cli"
-rescue LoadError => e
-  puts <<-HTML
-  </pre>
-  <div class="message">
-    <div class="error">Please install engineyard gem</div>
-    <pre>gem install engineyard</pre>
-  </div>
-  HTML
-  exit
-end
-
-unless ENV['TM_PROJECT_DIRECTORY'] && File.exist?(ENV['TM_PROJECT_DIRECTORY'])
-  puts <<-HTML
-  </pre>
-  <div class="message">
-    <div class="error">Run only from a project</div>
-    <p>The Engine Yard commands are for git-based, rack/rails projects</p>
-  </div>
-  HTML
-  exit
-end  
-
-ENV['THOR_SHELL'] = 'HTML'
-FileUtils.cd(ENV['TM_PROJECT_DIRECTORY'])
-
-begin
-  EY::CLI.start
-rescue EY::Error => e
-  EY.ui.print_exception(e)
-  exit(1)
-rescue Interrupt => e
-  puts
-  EY.ui.print_exception(e)
-  EY.ui.say("Quitting...")
-  exit(1)
-end
+fix_load_paths
+render_header
+setup_ok?
+run_command
---
+++ Support/lib/ey_command_preparation.rb
@@ -0,0 +1,62 @@
+module EyCommandHelper
+  # Due to need for escape gem, which name clashes with Textmate Support libs,
+  # Moving the Text
+  def fix_load_path
+    tm_support = $:.select { |path| path =~ %r{SharedSupport/Support/lib} }.first
+    $:.reject! { |path| path =~ %r{SharedSupport/Support/lib} }
+    $: << tm_support if tm_support
+  end
+  
+  # Render output header
+  def render_header
+    view = File.dirname(__FILE__) + "/../views/results_prefix.html"
+    print File.read(view)
+    $stdout.flush
+  end
+  
+  def setup_ok?
+    require "fileutils"
+    begin
+      require "rubygems"
+      require "engineyard/cli"
+    rescue LoadError => e
+      puts <<-HTML
+      </pre>
+      <div class="message">
+        <div class="error">Please install engineyard gem</div>
+        <pre>gem install engineyard</pre>
+      </div>
+      HTML
+      exit
+    end
+
+    unless ENV['TM_PROJECT_DIRECTORY'] && File.exist?(ENV['TM_PROJECT_DIRECTORY'])
+      puts <<-HTML
+      </pre>
+      <div class="message">
+        <div class="error">Run only from a project</div>
+        <p>The Engine Yard commands are for git-based, rack/rails projects</p>
+      </div>
+      HTML
+      exit
+    end  
+  end
+  
+  # Do the work from engineyard gem; standard out goes straight to command output
+  def run_command
+    ENV['THOR_SHELL'] = 'HTML'
+    FileUtils.cd(ENV['TM_PROJECT_DIRECTORY'])
+
+    begin
+      EY::CLI.start
+    rescue EY::Error => e
+      EY.ui.print_exception(e)
+      exit(1)
+    rescue Interrupt => e
+      puts
+      EY.ui.print_exception(e)
+      EY.ui.say("Quitting...")
+      exit(1)
+    end
+  end
+end
