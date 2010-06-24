ae5b894c83869eff80e01a447d42ed2c298e904a
Dr Nic Williams <drnicwilliams@gmail.com>
2010-06-24 11:56:09 +1000

    creating a fake app for the cuke features

--- Support/features/step_definitions/project_steps.rb
+++ Support/features/step_definitions/project_steps.rb
@@ -1,7 +1,15 @@
-Given /^I am in an application$/ do
-  
+Given /^I am in an application folder$/ do
+  in_tmp_folder do
+    FileUtils.cp_r(File.dirname(__FILE__) + "/../fixtures/someapp", "myapp")
+  end
+  setup_active_project_folder "myapp"
 end
 
 Given /^it is a git repository with a remote$/ do
-  pending # express the regexp above with the code you wish you had
+  in_project_folder do
+    %x[ git init ]
+    %x[ git add . ]
+    %x[ git commit -m "initial commit" ]
+    %x[ git remote add origin git@github.fake:/drnic/somefakerepo.git ]
+  end
 end
